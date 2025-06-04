afbfc718fa55864a03cddc8adc74a2c309e1b1ed
ianalexmac <iamacdougall@alaska.edu>
2025-06-04 13:28:22 -0800

    refactor csv conversion script

--- src/workbooks_xlsx_to_csv.py
+++ src/workbooks_xlsx_to_csv.py
@@ -1,47 +1,48 @@
 import pandas as pd
 
-annual_sales = pd.read_excel(
-    'workbooks/Energy_Stats_Financial_Tables.xlsx', 
-    sheet_name = 'Annual Sales FINAL 06262023',
-    header = 0)
-annual_sales.to_csv('workbooks_csv/annual_sales.csv', index = False)
+def read_and_clean(file, sheet_name, output_csv):
+    # Read the Excel sheet
+    df = pd.read_excel(file, sheet_name=sheet_name)
+    
+    # Drop columns where all values are NaN
+    df_clean = df.dropna(axis=1, how='all')
+    
+    # Save to CSV without the index column
+    df_clean.to_csv(output_csv, index=False)
 
 
-annual_operations = pd.read_excel(
-    'workbooks/Energy_Stats_Generation_Tables.xlsx',
-    sheet_name = 'AnnualOperationsData 2023-11-13',
-    header = 0
+read_and_clean(
+    file='workbooks/Energy_Stats_Financial_Tables.xlsx',
+    sheet_name='Annual Sales FINAL 06262023',
+    output_csv='workbooks_csv/annual_sales.csv'
 )
-annual_operations.to_csv('workbooks_csv/annual_operations.csv', index = False)
 
-
-monthly_generation = pd.read_excel(
-    'workbooks/Energy_Stats_Generation_Tables.xlsx',
-    sheet_name = 'Monthly Gen 2001-2021',
-    header = 0
+read_and_clean(
+    file='workbooks/Energy_Stats_Generation_Tables.xlsx',
+    sheet_name='AnnualOperationsData 2023-11-13',
+    output_csv='workbooks_csv/annual_operations.csv'
 )
-monthly_generation.to_csv('workbooks_csv/monthly_generation.csv', index = False)
 
-
-interties = pd.read_excel(
-    'workbooks/Energy_Stats_Infrastructure_2021.xlsx',
-    sheet_name = 'LOOKUP INTERTIES 2023-11-08',
-    header = 0
+read_and_clean(
+    file='workbooks/Energy_Stats_Generation_Tables.xlsx',
+    sheet_name='Monthly Gen 2001-2021',
+    output_csv='workbooks_csv/monthly_generation.csv'
 )
-interties.to_csv('workbooks_csv/interties.csv', index = False)
 
-
-infrastructure = pd.read_excel(
-    'workbooks/Energy_Stats_Infrastructure_2021.xlsx',
-    sheet_name = 'Infrastructure FINAL 2023-11-13',
-    header = 0
+read_and_clean(
+    file='workbooks/Energy_Stats_Infrastructure_2021.xlsx',
+    sheet_name='LOOKUP INTERTIES 2023-11-08',
+    output_csv='workbooks_csv/interties.csv'
 )
-infrastructure.to_csv('workbooks_csv/infrastructure.csv', index = False)
 
-
-plants = pd.read_excel(
-    'workbooks/Energy_Stats_Infrastructure_2021.xlsx',
-    sheet_name = 'LOOKUP PLANTS 2023-11-13',
-    header = 0
+read_and_clean(
+    file='workbooks/Energy_Stats_Infrastructure_2021.xlsx',
+    sheet_name='Infrastructure FINAL 2023-11-13',
+    output_csv='workbooks_csv/infrastructure.csv'
 )
-plants.to_csv('workbooks_csv/plants.csv', index = False)
+
+read_and_clean(
+    file='workbooks/Energy_Stats_Infrastructure_2021.xlsx',
+    sheet_name='LOOKUP PLANTS 2023-11-13',
+    output_csv='workbooks_csv/plants.csv'
+)
